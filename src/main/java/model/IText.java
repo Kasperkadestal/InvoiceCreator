bb23022f03278dc09a80e9beed3f991cc0a98091
Kasper Kadestål <84015423+Kasperkadestal@users.noreply.github.com>
2022-07-18 14:08:14 +0200

    0.2

--- src/main/java/model/IText.java
+++ src/main/java/model/IText.java
@@ -20,23 +20,38 @@ public class IText {
         String title = "Offert | " + info[0];
         PdfReader reader = new PdfReader("pdf/Offert_HWG_Golf.pdf");
         PdfStamper stamper = new PdfStamper(reader, new FileOutputStream("pdf/Offert_HWG_Golf_Modified.pdf")); // output PDF
-        BaseFont bf = BaseFont.createFont("fonts/Futura Heavy font.ttf", BaseFont.IDENTITY_H, BaseFont.EMBEDDED);
         System.out.println("Size: " + reader.getPageSize(1));
 
-        //loop on pages (1-based)
-        for (int i=1; i<=reader.getNumberOfPages(); i++){
+        // get object for writing over the existing content;
+        // you can also use getUnderContent for writing in the bottom layer
+        PdfContentByte over = stamper.getOverContent(1);
 
-            // get object for writing over the existing content;
-            // you can also use getUnderContent for writing in the bottom layer
-            PdfContentByte over = stamper.getOverContent(i);
+        // write title
+        over.beginText();
+        over.setRGBColorFill(8, 76, 52);
+        over.setFontAndSize(BaseFont.createFont("fonts/Futura Heavy font.ttf", BaseFont.IDENTITY_H, BaseFont.EMBEDDED), 24);
+        over.showTextAligned(PdfContentByte.ALIGN_CENTER, title.replaceAll("å", "\u00E5").replaceAll("Å", "\u212B").replaceAll("ä", "\u00E4").replaceAll("Ä", "\u00C4").replaceAll("ö", "\u00F6").replaceAll("Ö", "\u00D6"), (float) (595.276 / 2), 525, 0);
+        over.endText();
 
-            // write text
-            over.beginText();
-            over.setRGBColorFill(8,76,52);
-            over.setFontAndSize(bf, 24);    // set font and size
-            over.showTextAligned(PdfContentByte.ALIGN_CENTER,title.replaceAll("å","\u00E5").replaceAll("Å","\u212B").replaceAll("ä","\u00E4").replaceAll("Ä","\u00C4").replaceAll("ö","\u00F6").replaceAll("Ö","\u00D6"), (float) (595.276/2),525,0);  // set text
-            over.endText();
+        // write content
+        over.beginText();
+        over.setRGBColorFill(0, 0, 0);
+        over.setFontAndSize(BaseFont.createFont("fonts/Futura Book font.ttf", BaseFont.IDENTITY_H, BaseFont.EMBEDDED), 12);
+        int y = 437;
+        for (int i = 0; i < 8; i++) {
+            over.setTextMatrix(160,y-(i*20));
+            over.showText("Gran Canaria");
         }
+        over.setFontAndSize(BaseFont.createFont("fonts/futura medium bt.ttf", BaseFont.IDENTITY_H, BaseFont.EMBEDDED), 12);
+        over.setTextMatrix(92,y-(9*20));
+        over.showText("Köpenhamn - Gran Canaria | SAS".replaceAll("å", "\u00E5").replaceAll("Å", "\u212B").replaceAll("ä", "\u00E4").replaceAll("Ä", "\u00C4").replaceAll("ö", "\u00F6").replaceAll("Ö", "\u00D6"));
+        over.setFontAndSize(BaseFont.createFont("fonts/Futura Book font.ttf", BaseFont.IDENTITY_H, BaseFont.EMBEDDED), 12);
+        over.setTextMatrix(92,y-(10*20));
+        over.showText("SK2837 18DEC CPH-LPA 0805- 1230");
+        over.setTextMatrix(92,y-(11*20));
+        over.showText("SK2837 18DEC CPH-LPA 0805- 1230");
+
+        over.endText();
 
         stamper.close();
     }
